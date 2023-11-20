0cc19dc0be4b94db00d45b60c88e06b8826689b4
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-11-20 15:50:50 +0700

    .commit

--- MyApp3.py
+++ MyApp3.py
@@ -42,3 +42,31 @@ splen = st.number_input("กรุณาเลือกข้อมูล Age", 
 splen = st.number_input("กรุณาเลือกข้อมูล Gender", step=1, format="%d")
 splen = st.number_input("กรุณาเลือกข้อมูล EverBenched", step=1, format="%d")
 splen = st.number_input("กรุณาเลือกข้อมูล ExperienceInCurrentDomain", step=1, format="%d")
+
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+if st.button("ทำนายผล"):
+    X= raw_data.drop(columns='LeaveOrNot')
+    y=raw_data['LeaveOrNot']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=20, test_size=0.3)
+
+    rf = RandomForestClassifier()
+    rf.fit(X_train, y_train)
+
+    #ข้อมูล input สำหรับทดลองจำแนกข้อมูล
+   x_input = np.array([[Education, JoiningYear, City, PaymentTier, Age, Gender, EverBenched, ExperienceInCurrentDomain]])
+    # เอา input ไปทดสอบ
+   st.write(rf.predict(X_test))
+   out=rf.predict(X_test)
+
+    if out[0]==0:
+      #st.image("./pic/iris.jpg")
+      st.header("อยู่ต่อ")
+    else:
+      #st.image("./pic/iris1.jpg")  
+      st.header("มีแนวโน้มว่าจะลาออก")
+    st.button("ไม่ทำนายผล")
+else :
+    st.button("ไม่ทำนายผล")
